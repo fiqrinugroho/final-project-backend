00c4fd1578509df1fa4c9857373bf8fe7758600b
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-11-24 16:04:35 +0700

    add repository for api register

--- app/repositories/authRepository.js
+++ app/repositories/authRepository.js
@@ -6,6 +6,20 @@ const createUser = (createArgs) => {
   return user.create(createArgs);
 }
 
+// untuk mencari data sesuai dengan email user
+const findEmail = (email) => {
+    // cari user berdasarkan email
+    const find = user.findOne({
+        where: {
+          email,
+        },
+        include: {
+          model: role,
+        }
+      });
+    return find
+}
+
 // untuk mencari data user dan profile
 const findUser = (email) => {
     // cari user berdasarkan email
@@ -23,7 +37,13 @@ const findUser = (email) => {
     return find
 }
 
+const addProfile = (userId, fullName) => {
+  return profile.create({ fullName, userId})
+}
+
 module.exports = {
     createUser,
+    findEmail,
+    addProfile,
     findUser,
 }
