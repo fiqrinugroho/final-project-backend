a9f52b94d9dd1bdf62c0b694948c65b6fd3ed96a
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-11-24 13:30:53 +0700

    add seeders for admin user & role

---
+++ database/seeders/20221123130303-user.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     return queryInterface.bulkInsert("users",[
+      {
+        fristName: "fiqri",
+        lastName: "nugroho",
+        email: "fiqri@mail.com",
+        password: bcrypt.hashSync("qwerty123", 10),
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
