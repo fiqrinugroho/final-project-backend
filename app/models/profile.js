1542cdc5f0940d9daf1fa8f8baa72720a96c658c
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-11-24 15:11:15 +0700

    fix associate to user

--- app/models/profile.js
+++ app/models/profile.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
      static associate(models) {
-      this.belongsTo(models.profile, {
+      this.belongsTo(models.user, {
         foreignKey: 'userId',
       })
 
