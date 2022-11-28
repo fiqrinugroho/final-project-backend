ddefb5c087081e676d20d4773ff00fb597179582
zidane arvito <zidanevito@gmail.com>
2022-11-29 03:11:48 +0700

    setup swagger-ui

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 
 const auth = require("./auth");
+const docs = require('./docs');
 
 // API auth
 router.use("/api/auth/", auth);
+router.use('/api-docs', docs);
 
 module.exports = router;
