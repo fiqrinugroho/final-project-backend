9848646f2adfdc2e50ac22bae479f7a8ceaf6126
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-12-08 13:46:38 +0700

    add endpont for API create and get all company

---
+++ config/routes/company.js
@@ -0,0 +1,23 @@
+const router = require("express").Router();
+
+// controller
+const company = require("../../app/controllers/companyController");
+
+// middleware
+const Authentication = require("../../middlewares/authenticate");
+const isAdmin = require("../../middlewares/isAdmin");
+const Uploader = require("../../middlewares/uploader");
+
+router.post(
+  "/create",
+  Authentication,
+  isAdmin,
+  Uploader.single("image"),
+  company.createCompany
+);
+router.get("/", company.getCompany);
+router.get("/:id", company.getCompanyById);
+// router.put("/update/:id", Authentication, isAdmin, company.updateCompany);
+// router.delete("/delete/:id", Authentication, isAdmin, company.deleteCompany);
+
+module.exports = router;
