17fb114c83822f422ec50336765b21f6078e2ff3
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-12-09 20:17:26 +0700

    eslint fix

--- config/routes/user.js
+++ config/routes/user.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 
-
 const user = require("../../app/controllers/userController");
 
 // middleware
@@ -8,7 +7,12 @@ const Authentication = require("../../middlewares/authenticate");
 const Uploader = require("../../middlewares/uploader");
 
 router.get("/", Authentication, user.getUserById);
-router.put("/update/", Authentication, Uploader.single("image"), user.updateUser);
+router.put(
+  "/update/",
+  Authentication,
+  Uploader.single("image"),
+  user.updateUser
+);
 router.delete("/:id", Authentication, user.deleteUser);
 
 module.exports = router;
