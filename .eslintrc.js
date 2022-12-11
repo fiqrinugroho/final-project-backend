43e4b4171b8e4bfa6305480d1ae1a5b38cbc2505
imam.hermawan <imam.hermawan@gramedia.id>
2022-12-11 19:33:59 +0700

    add new rule eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,12 +12,27 @@ module.exports = {
     ecmaVersion: "latest",
   },
   rules: {
-    "prettier/prettier": ["error"],
-    "consistent-return": "off",
-    "func-names": "off",
+    // "prettier/prettier": ["error"],
+    "indent": ["error", 2],
+    "comma-dangle": ["error", {
+      "arrays": "always",
+      "objects": "always",
+      "imports": "never",
+      "exports": "never",
+      "functions": "never"
+    }],
+    "quotes": ["error", "double"],
+    "max-len": ["error", {
+      "code": 80,
+      "tabWidth": 2,
+      "ignoreComments": true,
+      "ignoreTrailingComments": true,
+      "ignoreStrings": true
+    }],
+    "jest/expect-expect": "off",
     "no-console": "error",
-    "no-underscore-dangle": "off",
     "no-unused-labels": "error",
     "no-unused-vars": "error",
+    "semi": ["error", "always"]
   },
 };
