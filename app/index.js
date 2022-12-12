91e9ffbb4d312e13ad63661c79be9e6882953099
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-12-12 23:02:35 +0700

    set up for unit test using jest and supertest

---
+++ app/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const router = require("../config/routes/index");
+
+const app = express();
+
+app.use(morgan("common"));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+module.exports = app;
