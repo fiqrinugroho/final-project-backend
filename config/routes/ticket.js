56706d3b533d93572c263bb423b2037a559a2927
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-12-15 16:27:36 +0700

    add endpoint get all data ticket

--- config/routes/ticket.js
+++ config/routes/ticket.js
@@ -8,7 +8,7 @@ const Authentication = require("../../middlewares/authenticate");
 const isAdmin = require("../../middlewares/isAdmin");
 
 router.post("/create", Authentication, isAdmin, ticket.createTicket);
-// router.get("/", ticket.getTicket);
+router.get("/", ticket.getTicket);
 // router.get("/:id", ticket.getTicketById);
 // router.put("/update/:id", Authentication, isAdmin, ticket.updateTicket);
 // router.delete("/delete/:id", Authentication, isAdmin, ticket.deleteTicket);
