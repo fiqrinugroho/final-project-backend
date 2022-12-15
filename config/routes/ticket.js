081ca58b93da648fe697f1336f5cdc5ff4fafe87
Muhammad Fiqri Nugroho <fiqrynugroho@gmail.com>
2022-12-15 17:27:56 +0700

    add api update & delete ticket

--- config/routes/ticket.js
+++ config/routes/ticket.js
@@ -10,7 +10,7 @@ const isAdmin = require("../../middlewares/isAdmin");
 router.post("/create", Authentication, isAdmin, ticket.createTicket);
 router.get("/", ticket.getTicket);
 router.get("/:id", ticket.getTicketById);
-// router.put("/update/:id", Authentication, isAdmin, ticket.updateTicket);
-// router.delete("/delete/:id", Authentication, isAdmin, ticket.deleteTicket);
+router.put("/update/:id", Authentication, isAdmin, ticket.updateTicket);
+router.delete("/delete/:id", Authentication, isAdmin, ticket.deleteTicket);
 
 module.exports = router;
