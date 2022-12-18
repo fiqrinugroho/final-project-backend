8e5425d452cd58e15999946a14ad7a61bd9a9edf
zidane arvito <zidanevito@gmail.com>
2022-12-18 14:31:57 +0700

    add delete airplane test

--- tests/airplane.api.test.js
+++ tests/airplane.api.test.js
@@ -42,3 +42,11 @@ describe("API Update Airplane Data By Id", () => {
     expect(response.statusCode).toBe(401);
   });
 });
+
+describe("API Delete Airplane", () => {
+  it("Unauthorized", async () => {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJ2aXRvQG1haWwuY29tIiwicm9sZUlkIjoyLCJpYXQiOjE2Njk2NjM2MDB9.t-mS8RHauM7M5fiIGbXRDaJg7pVE2O82HwfTyY7Z98E";
+    const response = await request(app).delete("/api/airplane/delete/2").set("Authorization", token);
+    expect(response.statusCode).toBe(401);
+  });
+});
