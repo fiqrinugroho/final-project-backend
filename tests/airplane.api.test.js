7fac163dfe614411dc75a2a0fdb3d2202cca1703
zidane arvito <zidanevito@gmail.com>
2022-12-18 14:05:38 +0700

    add get airplane data by id test

--- tests/airplane.api.test.js
+++ tests/airplane.api.test.js
@@ -3,9 +3,16 @@ const app = require("../app/index");
 const dotenv = require("dotenv");
 dotenv.config();
 
-describe("API Get All Airplane", () => {
+describe("API Get All Airplane Data", () => {
   it("success", async () => {
     const response = await request(app).get("/api/airplane/");
+    expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("API Get Airplane Data By Id", () => {
+  it("Success", async () => {
+    const response = await request(app).get("/api/airport/2");
     expect(response.statusCode).toBe(200);
   });
 });
