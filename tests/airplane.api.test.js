48f222af4af77e4896ed16ad123050cc3741575d
zidane arvito <zidanevito@gmail.com>
2022-12-18 14:29:24 +0700

    add update airplane data by id test

--- tests/airplane.api.test.js
+++ tests/airplane.api.test.js
@@ -10,9 +10,35 @@ describe("API Get All Airplane Data", () => {
   });
 });
 
+describe("API Create Airplane", () => {
+  const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6ImZpcXJpIiwiZW1haWwiOiJmaXFyaUBtYWlsLmNvbSIsInJvbGVJZCI6MSwiaWF0IjoxNjcwMzMyOTU3fQ.wXPmJ2TXeprs3wcw_8u4RONLiUm_KG9zcboaAibyooo";
+  it("Unauthorized", async () => {
+    const airplane = {
+      airplaneName: "Boeing 737-800",
+      airplaneCode: "1212",
+      companyName: "LION AIR",
+    };
+    const response = await request(app).post("/api/airplane/create").set("Authorization", token).send(airplane);
+    expect(response.statusCode).toBe(401);
+  });
+});
+
 describe("API Get Airplane Data By Id", () => {
   it("Success", async () => {
-    const response = await request(app).get("/api/airport/2");
+    const response = await request(app).get("/api/airplane/2");
     expect(response.statusCode).toBe(200);
+  });
+});
+
+describe("API Update Airplane Data By Id", () => {
+  it("Unauthorized", async () => {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJ2aXRvQG1haWwuY29tIiwicm9sZUlkIjoyLCJpYXQiOjE2Njk2NjM2MDB9.t-mS8RHauM7M5fiIGbXRDaJg7pVE2O82HwfTyY7Z98E";
+    const airplane = {
+      airplaneName: "Boeing 737-800",
+      airplaneCode: "1212",
+      companyName: "LION AIR",
+    };
+    const response = await request(app).put("/api/airplane/update/2").set("Authorization", token).send(airplane);
+    expect(response.statusCode).toBe(401);
   });
 });
