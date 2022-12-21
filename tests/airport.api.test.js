9f01c6655c9ebf268a5c2fc491a8634742515066
zidane arvito <zidanevito@gmail.com>
2022-12-21 10:27:57 +0700

    add invalid token test for airport test

--- tests/airport.api.test.js
+++ tests/airport.api.test.js
@@ -95,16 +95,21 @@ describe("API Update Airport By Id", () => {
     expect(response.statusCode).toBe(200);
   });
   
-  it("Unauthorized", async () => {
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJ2aXRvQG1haWwuY29tIiwicm9sZUlkIjoyLCJpYXQiOjE2Njk2NjM2MDB9.t-mS8RHauM7M5fiIGbXRDaJg7pVE2O82HwfTyY7Z98E";
+  it("Invalid Token", async () => {
+    const token = "";
     const airport = {
       airportName: "soekarno hatta",
       city: "tangerang",
       cityCode: "CGK",
     };
-    const response = await request(app).put("/api/airport/update/2").set("Authorization", token).send(airport);
+    const response = await request(app)
+      .put("/api/airport/update/2")
+      .set("Authorization", 'Bearer ' + token)
+      .send(airport);
     expect(response.statusCode).toBe(401);
   });
+
+  
 });
 
 describe("API Delete Airport", () => {
