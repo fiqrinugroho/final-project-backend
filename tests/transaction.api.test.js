be8b848c53cb96c1a5dda6a1d8de56834c1d4c29
zidane arvito <zidanevito@gmail.com>
2022-12-30 16:06:31 +0700

    add cancel and update transaction

--- tests/transaction.api.test.js
+++ tests/transaction.api.test.js
@@ -95,3 +95,58 @@ describe("API Get Transaction Data By Id", () => {
   });
 });
 
+describe("API Update transaction data by Id", () => {
+  it("Success Update Transaction", async () => {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJ2aXRvQG1haWwuY29tIiwicm9sZUlkIjoyLCJpYXQiOjE2Njk2NjM2MDB9.t-mS8RHauM7M5fiIGbXRDaJg7pVE2O82HwfTyY7Z98E";
+    const transaction = {
+      ticketGo: 1,
+      ticketBack: 2,
+      tripId: 2,
+      firstName: "udin",
+      lastName: "gambut",
+      NIK: "32151314230402",
+      brithDate: "2022-1-21",
+    };
+    const response = await request(app)
+      .put("/api/transaction/update/1")
+      .set("Authorization", "Bearer " + token)
+      .send(transaction);
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("Invalid Token", async () => {
+    const token = "";
+    const transaction = {
+      ticketGo: 1,
+      ticketBack: 2,
+      tripId: 2,
+      firstName: "udin",
+      lastName: "gambut",
+      NIK: "32151314230402",
+      brithDate: "2022-1-21",
+    };
+    const response = await request(app)
+      .put("/api/transaction/update/1")
+      .set("Authorization", "Bearer " + token)
+      .send(transaction);
+    expect(response.statusCode).toBe(401);
+  });
+});
+
+describe("API cancel transaction data by Id", () => {
+  it("Success Cancel Transaction", async () => {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NiwiZW1haWwiOiJ2aXRvQG1haWwuY29tIiwicm9sZUlkIjoyLCJpYXQiOjE2Njk2NjM2MDB9.t-mS8RHauM7M5fiIGbXRDaJg7pVE2O82HwfTyY7Z98E";
+    const response = await request(app)
+      .put("/api/transaction/cancel/1")
+      .set("Authorization", "Bearer " + token)
+    expect(response.statusCode).toBe(200);
+  });
+
+  it("Invalid Token", async () => {
+    const token = "";
+    const response = await request(app)
+      .put("/api/transaction/cancel/1")
+      .set("Authorization", "Bearer " + token);
+    expect(response.statusCode).toBe(401);
+  });
+});
