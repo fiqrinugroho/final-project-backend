7d8e1f4514d4b01c799eedf44871707eca38c094
yamiyugi19 <95992200+yamiyugi19@users.noreply.github.com>
2023-01-01 08:18:28 +0700

    Fix relationship

--- app/repositories/whistlistRepository.js
+++ app/repositories/whistlistRepository.js
@@ -1,46 +1,137 @@
-const { user, ticket, whistlist, } = require("../models");
+const { airplane, airport, company, user, ticket, whistlist, } = require("../models");
 
 const createWish = async (newWish) => {
+
   return await whistlist.create(newWish);
+
 };
 
 const getWish= () => {
+
   return whistlist.findAll({
+
     include: [
+
       {
+
         model: user,
+
       },
+
       {
+
         model: ticket,
+
+        include: [
+
+          {
+
+            model: airport, 
+
+            as:"origin",
+
+          },
+
+          {
+
+            model: airport, 
+
+            as:"destination",
+
+          },
+
+          {
+
+            model: airplane, attributes: { exclude: ["seatCapacity",], },
+
+            include: company,
+
+          },
+
+        ],
+
       },
+
     ],
+
   });
+
 };
 
 const findWish = (userId) => {
+
   const find = whistlist.findAll({
+
     where: {
+
       userId,
+
     },
+
     include: [
+
       {
+
         model: user,
+
       },
+
       {
+
         model: ticket,
+
+        include: [
+
+          {
+
+            model: airport, 
+
+            as:"origin",
+
+          },
+
+          {
+
+            model: airport, 
+
+            as:"destination",
+
+          },
+
+          {
+
+            model: airplane, attributes: { exclude: ["seatCapacity",], },
+
+            include: company,
+
+          },
+
+        ],
+
       },
+
     ],
+
   });
+
   return find;
+
 };
 
 const deleteWish = async (id) => {
+
   return await whistlist.destroy({ where: { id, }, });
+
 };
 
 module.exports = {
+
   createWish,
+
   getWish,
+
   findWish,
+
   deleteWish,
+
 };
